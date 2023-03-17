a70867d8d7ba13f751f2db5f81f07fcdc0d4c343
Trevor Harmon <trevor.harmon@shopify.com>
2023-03-17 17:03:35 -0400

    Updated gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,13 +2,9 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `zephr-lang`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Zephyr`,
+    siteUrl: `https://zephyr-demo.netlify.app/`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-postcss",
     {
